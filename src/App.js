2d1a33748460fb67a813a3cd0e28aa058aa48565
prakash2 <prakashkusari0@gmail.com>
2026-02-03 11:23:29 -0600

    initial of  the project

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        {/* Logo */}
+        <img
+          
+          className="App-logo"
+          alt="Credence logo"
+        />
+
+        {/* Title */}
+        <h1>Credence</h1>
+
+        {/* Tagline */}
+        <p className="subtitle">
+          Blockchain-powered academic certificate verification.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        {/* Actions */}
+        <div className="button-group">
+          <button className="primary-btn">Student Portal</button>
+          <button className="secondary-btn">University Portal</button>
+          <button className="outline-btn">Verify Certificate</button>
+        </div>
       </header>
     </div>
   );
